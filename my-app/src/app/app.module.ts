55b34771ea30969568ecaec6e0314045afe811ea
Elizabeth Sanchez <iza_707@hotmail.com>
2017-08-07 09:42:28 -0500

    Continuando..

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { AdminModule } from './admin/admin.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BackendApiService } from './backend-api.service';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { ConvertToEuroPipe } from './convert-to-euro/convert-to-euro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    ConvertToEuroPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AdminModule
   ],
-  providers: [],
+  providers: [BackendApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ my-app/src/app/site-header/site-header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site-header',
+  template: `
+    <p>
+      site-header Works!
+    </p>
+  `,
+  styles: []
+})
+export class SiteHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
